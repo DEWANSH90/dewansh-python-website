299a8f95813659f3d368602b56fce91555e7fb90
DEWANSH90 <145478613+DEWANSH90@users.noreply.github.com>
2023-09-21 19:35:15 +0000

    dynamic changes

--- app.py
+++ app.py
@@ -2,9 +2,22 @@ from flask import Flask , render_template
 
 app = Flask(__name__)
 
+jobs1 = [
+  {
+  'id': 1 ,
+  'role':'DataEngineer',
+  'location':'Bengaluru,india'
+  },
+  {
+  'id': 2 ,
+  'role':'DataEngineer',
+  'location':'Pune,india'
+    
+  }
+]
 @app.route("/")
 def helloworld():
-   return render_template('home.html')
+   return render_template('home.html', jobs=jobs1)
 
 if __name__ == "__main__" :
   app.run(host='0.0.0.0' , debug=True)
